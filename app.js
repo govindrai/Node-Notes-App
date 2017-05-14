913906efecf4e209fc8dc086750482e0e32273d0
“Vilde <vilde@vevatne.no>
2017-05-14 15:19:46 -0700

    adds delete and add note functions

--- app.js
+++ app.js
@@ -1,18 +1,45 @@
+'use strict';
 // we want to make a notes app
 // we want to be able to add notes, delete notes, list all our notes
 // we want to do this all in the command line, so we need to use ARGV
 // since we wont have a database we want to store our notes in a json file
 
-console.log("Inside App.js")
+console.log("Inside App.js");
 
-var fs = require('fs')
+// system modules
+const fs = require('fs');
 
-try {
-	var notes = fs.readFileSync('notesdb.json')
-	console.log(notes)
-} catch(e) {
-	fs.openSync('notesdb.json', 'w')
-	console.log("sorry I couldn't read the file!")
+// npm modules
+const yargs = require('yargs');
+
+// user modules
+const notes = require('./notes');
+
+// notes.add_note("Our second note", "This is a description of our second note")
+
+// try {
+// 	var notes = fs.readFileSync('notesdb.json');
+// 	console.log(notes);
+// } catch(e) {
+// 	fs.openSync('notesdb.json', 'w');
+// 	console.log("sorry I couldn't read the file!");
+// }
+
+var args = yargs.argv;
+console.log(args)
+console.log("COMMAND IS :", args._[0]); 
+var command = args._[0];
+
+
+switch (command) {
+	case 'delete':
+		console.log("Okay let's remove a note!");
+		notes.delete_note(args.title);
+		break;
+	case 'add':
+		console.log("Okay let's add a note!");
+		notes.add_note(args.title, args.description)
+		break;
+	default:
+		console.log("What the french you talking about");
 }
-
-console.log(process.argv)
---
+++ notes.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const fs = require('fs');
+
+function read_notes() {
+	var file = fs.readFileSync('notesdb.json');
+	return JSON.parse(file);
+}
+
+function create_file() {
+	console.log("the file doesn't exist my man. Let's create the file");
+	fs.openSync('notesdb.json','w');
+	return [];
+}
+
+function write_file(notes) {
+	fs.writeFileSync('notesdb.json', JSON.stringify(notes));
+}
+
+function add_note(title, description) {
+	var note = {
+		title: title,
+		description: description
+	};
+	
+	try {
+		var notes = read_notes();
+	} catch(e) {
+		var notes = create_file();
+	}
+
+	notes.push(note);
+
+	write_file(notes);
+
+}
+
+function delete_note(title) {
+	try {
+		var notes = read_notes();
+		var deletedDescription;
+		var new_notes = notes.filter((note) => {
+			if (note.title != title) {
+				return true;
+			} else {
+				deletedDescription = note.description;
+			}
+		}); 
+		write_file(new_notes);
+		if (new_notes.length == notes.length){
+			console.log("There's no note in our system with title " + title);
+		} else {
+			console.log("We deleted the note with the following description: " + deletedDescription);
+		}
+	} catch (e) {
+		// console.log(e);
+		console.log("There are no notes in this system. You need to create a note before you can delete.")
+	}
+}
+
+function list_notes() {
+
+}
+
+function say_hello() {
+
+}
+
+module.exports = {
+	add_note,
+	delete_note,
+	list_notes
+}
+
